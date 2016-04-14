d57c2cceba455504aa499dbbb8e108948b95e355
ricardoJmarques <p_ricardo@hotmail.com>
2016-04-14 03:05:06 +0100

    API-facturacao v0.1 terminada

--- API-Facturacao.c
+++ API-Facturacao.c
@@ -40,11 +40,10 @@ catFacturacao iniciaCatFacturacao(){
 }
 
 catFacturacao insereVenda(catFacturacao cat, catClientes cli, catProdutos pro, char *venda){
-  int i, j;
+  int i;
   char v[7][7]; /*char produtoID[7];char preço[7];char qtd[4];char PN[2];char cliente[6];char MES[3];char Filial[2];*/
   char *var;
   ppVenda pp;
-  
   i=0;
   var = strtok(venda, " ");
   strcpy(v[i] , var);
@@ -52,9 +51,15 @@ catFacturacao insereVenda(catFacturacao cat, catClientes cli, catProdutos pro, c
     i++;
     strcpy(v[i] , var);
   }
-  
+  i = ((atoi(v[6])-1) * TAMCAT) + (atoi(v[5]) - 1 );
   if (existeVenda(cat, v[0],  atoi(v[6]), atoi(v[5]))){
-    printf("Existe\n"); /* update venda */
+    pp = (ppVenda)retornaDados (cat[i].lista, v[0]);
+    if (v[3][0] == 'N'){
+        pp->qtdN += atoi(v[2]);
+      }
+      else{
+        pp->qtdP += atoi(v[2]);
+      }
   }
   else {
     if (existeProduto(pro, v[0]) && existeCliente(cli, v[4])){
@@ -73,7 +78,6 @@ catFacturacao insereVenda(catFacturacao cat, catClientes cli, catProdutos pro, c
         pp->qtdN = 0;
         pp->precoN = 0;
       }
-      i = ((atoi(v[6])-1) * TAMCAT) + (atoi(v[5]) - 1 );
       cat[i].lista = insertAVL(cat[i].lista, var, &cat[i].crescimento, pp);
       cat[i].tamanho++;
     }
