42049a1b8add8d0b4ec04830024b4951d297976e
padraoUM <zeroaceftw@gmail.com>
2016-04-21 18:32:51 +0100

    impressao a funcionar 5*

--- API-impressao.c
+++ API-impressao.c
@@ -3,116 +3,95 @@
 #include <ctype.h>
 #include <string.h>
 
-#define PageSize 150
+#if ((__APPLE__) || (__linux__)) /* Deteta se o SO é linux ou mac para usar comando de limpar a tela */
+#define SYSTEM 1
+#elif (_WIN32)                   /* Deteta se o SO é Windows para usar comando de limpar a tela */
+#define SYSTEM 0
+#escolher					     /* Nao limpa a tela de um sistema que não é ou UNIX ou Windows */
+#endif
 
-/*FAZER UMA STRUCT PARA CAMUFLAR O TIPO CHAR** */
+#define PageSize 35
 
-/*
-void imprimeString (char ** cat , int size);
-int verificaNumero(char *num, int min, int max);
-
-int main(){
-
-	char** str;
-	int i;
-	str = malloc(sizeof(char*));
-
-	for(i=0 ; i<18 ; i++){
-		str[i] = malloc(sizeof(char) * 5);
-		str[i] = "cyka blyat";
-	}
-
-	imprimeString(str , 18);
-}
-*/
-
-void imprimeString (char** cat , int size){
-	int i, numpagina,j, nump, k , controlo;
-	int cmd;
+void imprimeString (char ** cat , int size){
+	int i, numpagina,j, nump, k , controlo,l,controloDigito;
+	char* cmd;
 
 	if(size%PageSize != 0)
-		nump = (size/PageSize)+1;
-	else nump = size/PageSize;
+		nump = (size/PageSize)+2;
+	else nump = size/PageSize+1;
 
-	printf("%d\n", nump);
+	printf("numero de paginas = %d\n", nump);
 
-	for(numpagina=0; numpagina < nump ; numpagina++){
+	for(numpagina=1; numpagina < nump ; numpagina++){
 
-		printf("teste=%s\n", cat[0] );
+		if(controlo == 0)
+			numpagina--;
+
+
 		controlo=1;
 
-		printf("Proxima pagina [-1]\n");
-		printf("Pagina anterior [-2]\n");
+		printf("Proxima pagina [proxima]\n");
+		printf("Pagina anterior [anterior]\n");
 		printf("Saltar para a pagina [numero da pagina]\n");
-		printf("Sair [-3]\n");
-		scanf("%d" , &cmd);
+		printf("limpar ecra [clear]\n");
+		printf("Sair [exit]\n");
+		scanf("%s" , cmd);
 
-		/*
-		if(!strcmp(cmd,"exit"))
-			return;
-		else{
-			if(!strcmp(cmd,"exit"))
-				numpagina = numpagina-2;
+
+		for(l=0 , controloDigito =1; cmd[l]!='\0' && controloDigito ;l++){
+			if( ! isdigit(cmd[l])){
+				controloDigito = 0;
+			}
+		}
+
+		if(!controloDigito){
+			if(!strcmp(cmd , "exit"))
+				return;
 			else{
-				if(!strcmp(cmd,"proxima")){
-				nao faz nada 
+				if(!strcmp(cmd , "proxima")){
+					/*faz nada*/
 				}
 				else{
-					for(j=0,k=1 ; cmd[j]!='\n' && k ; j++){
-						if(!isdigit(cmd[j])){
-							k=0;
-							break;
+					if(!strcmp(cmd , "anterior")){
+						numpagina = numpagina -2;
+					}
+					else{
+						if(!strcmp(cmd , "clear")){
+							controlo = 0;
+							if (SYSTEM) system ("clear");
+							if (!SYSTEM) system ("cls");
 						}
-					}
-					if(k){
-						k=atoi(cmd);
-						if(k>0 && k<= nump)
-							numpagina = k-1;
-					}
-
-					else{ 
-						printf("comando invalido\n");
-						controlo = 0;
+						else{
+							printf("comando invalido\n");
+							controlo = 0;
+						}
 					}
 				}
 			}
 		}
-		*/
+		else{
+			k=atoi(cmd);
+			if(k>0 && k < nump){
+				numpagina = k;
+			}
 
-		switch(cmd){
-			case -2:
-				numpagina = numpagina -2;
-				break;
-
-			case -1:
-				break;
-
-			case -3:
-				return;
-
-			default :
-				if(cmd>0 && cmd<= nump){
-					numpagina = cmd;
-				}
-
-				else{ 
-					printf("numero de pagina invalido\n");
-					controlo=0;
-				}
-				break;
-
+			else {
+				printf("numero de pagina invalido\n");
+				controlo = 0;
+			}
 		}
 
 		while (getchar() != '\n' );
 
+		if(controlo == 1){
+			if (SYSTEM) system ("clear");
+			if (!SYSTEM) system ("cls");
+		}
+
 		printf("numpagina = %d\n", numpagina );
 
-		for(i=numpagina*PageSize , j=0; controlo && i<size && j<PageSize ; i++ , j++)
+		for(i=(numpagina-1)*PageSize , j=0; controlo && i<size && j<PageSize ; i++ , j++)
 			puts(cat[i]);
-
-		printf("i = %d\n", i );
-
-		printf("------------------------NOVA PAGINA-------------------------\n");
 
 	}
 
